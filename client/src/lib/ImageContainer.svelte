6b80283c8a8eb74f5e6f557ed2f9903d13fda5ee
Zachintosh <zlw11063@gmail.com>
2021-10-07 21:31:51 -0600

    Add a temporary onclick and interactive stylings

--- client/src/lib/ImageContainer.svelte
+++ client/src/lib/ImageContainer.svelte
@@ -1,8 +1,15 @@
 <script>
   export let image;
+  export const handleImageClick = (e) => {
+    console.log('Image clicked - eventually this will do something COOL');
+  };
 </script>
 
-<div class="image-container" style="background-image: url({image})">
+<div
+  class="image-container"
+  style="background-image: url({image})"
+  on:click="{handleImageClick}"
+>
   <!-- This isn't what we'll actually put here, I just wanted to show that copilot thought I was working in PHP -->
   <!-- <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" /> -->
 </div>
@@ -16,5 +23,11 @@
     background-position: center;
     background-size: cover;
     object-fit: fill;
+    cursor: pointer;
+    transition: filter 0.05s;
+  }
+
+  .image-container:hover {
+    filter: brightness(0.95);
   }
 </style>
