d62c77e771a0429a3d975c6ad9a6d68baf40b9dd
Zachary Williams <zlw11063@gmail.com>
2021-10-08 11:03:45 -0600

    Add basic UI components (#6)

---
+++ client/src/lib/ImageContainer.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let image;
+  export const handleImageClick = (e) => {
+    console.log('Image clicked - eventually this will do something COOL');
+  };
+</script>
+
+<div
+  class="image-container"
+  style="background-image: url({image})"
+  on:click="{handleImageClick}"
+>
+  <!-- This isn't what we'll actually put here, I just wanted to show that copilot thought I was working in PHP -->
+  <!-- <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" /> -->
+</div>
+
+<style>
+  .image-container {
+    height: 140px;
+    width: 140px;
+    border-radius: 12px;
+    background: #11111109;
+    background-position: center;
+    background-size: cover;
+    object-fit: fill;
+    cursor: pointer;
+    transition: filter 0.05s;
+  }
+
+  .image-container:hover {
+    filter: brightness(0.95);
+  }
+</style>
---
+++ client/src/lib/ImageGrid.svelte
@@ -0,0 +1,27 @@
+<script>
+  import ImageContainer from './ImageContainer.svelte';
+
+  const tempCat = 'https://i.ytimg.com/vi/jH7e1fDcZnY/maxresdefault.jpg';
+</script>
+
+<div class="grid">
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+  <ImageContainer image="{tempCat}" />
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    width: fit-content;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-gap: 4px;
+  }
+</style>
