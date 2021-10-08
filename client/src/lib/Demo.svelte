ed2f0581e9b617d6a78d08b907e1175e9b54c83e
Zachintosh <zlw11063@gmail.com>
2021-10-07 21:26:15 -0600

    Add basic UI components

---
+++ client/src/lib/Demo.svelte
@@ -0,0 +1,13 @@
+<!-- Component for centering the demo of this whole circus -->
+
+<div class="demo">
+  <slot />
+</div>
+
+<style>
+  .demo {
+    display: flex;
+    justify-content: center;
+    margin-top: 20vh;
+  }
+</style>
---
+++ client/src/lib/ImageContainer.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let image;
+</script>
+
+<div class="image-container" style="background-image: url({image})">
+  <!-- This isn't what we'll actually put here, I just wanted to show that copilot thought I was working in PHP -->
+  <!-- <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" /> -->
+</div>
+
+<style>
+  .image-container {
+    height: 140px;
+    width: 140px;
+    border-radius: 12px;
+    background: #11111109;
+    background-position: center;
+    background-size: cover;
+    object-fit: fill;
+  }
+</style>
